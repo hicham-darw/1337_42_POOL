1247438de6a81cba9ecafc525a73bf5cf190f66c
hicham-darw <hamoudihicham@gmail.com>
2025-01-31 23:41:32 +0100

    adding ex makefile

--- day02/ex06/ft_putnbr.c
+++ day02/ex06/ft_putnbr.c
@@ -1,24 +1,23 @@
 #include	<unistd.h>
 
+void ft_putchar(char c);
 
 void	ft_putnbr(int nb)
 {
-	int lnb, rnb;
-	int sign = -1;
+	unsigned int nbr;
+	unsigned int lnbr, rnbr;
 
-	if(nb >= 0)
-	{
-		lnb = (nb/10);
-		rnb = (nb%10)+48;
-		if(nb >= 10)
-		{
-			ft_putnbr(lnb);		
-		}
-		write(1, &rnb, 1);
+	if((unsigned int)nb > 0){
+		nbr = (unsigned int)(nb);
 	}
-	else
-	{
+	if(nb < 0){
 		write(1, "-", 1);
-		ft_putnbr(nb*sign);
+		nbr = (unsigned int)(nb * (-1));
 	}
+	lnbr = nbr/10;
+	rnbr = nbr%10;
+	if(nbr >= 10){
+		ft_putnbr(lnbr);
+	}
+	ft_putchar(rnbr+48);
 }
---
+++ day09/ex01/includes/libft.h
@@ -0,0 +1,32 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+#include	<unistd.h>
+#include	<fcntl.h>
+#include	<stdlib.h>
+
+int	ft_atoi(char *str);
+int	ft_iterative_power(int n, int power);
+void	ft_putchar(char c);
+void	ft_putnbr(int nbr);
+void	ft_putstr(char *str);
+int	ft_recursive_power(int n, int power);
+void	ft_sort_integer_table(int *tab, int size);
+int	ft_sqrt(int n);
+char	*ft_strcat(char *dst, char *src);
+int	ft_strcmp(char *s1, char *s2);
+char	*ft_strcpy(char *dst, char *src);
+int	ft_str_is_alpha(char *str);
+int	ft_str_is_numeric(char *str);
+int	ft_str_is_printable(char *str);
+int	ft_strlcat(char *dst, char *src, unsigned int size);
+int	ft_strlcpy(char *dst, char *src, unsigned int size);
+char	*ft_strlowcase(char *str);
+char	*ft_strncat(char *dst, char *src, int n);
+int	ft_strncmp(char *s1, char *s2, int n);
+char	*ft_strncpy(char *dst, char *src, int n);
+char	*ft_strrev(char *str);
+char	*ft_strstr(char *src, char *to_find);
+char	*ft_strupcase(char *str);
+void	ft_swap(char *a, char *b);
+
+#endif
