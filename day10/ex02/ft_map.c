666d59c88e1b1c46f7f4c89c3f5403e44c6ddb97
hicham-darw <hamoudihicham@gmail.com>
2025-02-08 22:45:51 +0100

    adding func ft_map.c

--- day10/ex02/ft_map.c
+++ day10/ex02/ft_map.c
@@ -2,13 +2,12 @@ int	*ft_map(int *tab, int length, int(*f)(int))
 {
 	int i;
 	i = 0;
-	int *new_tab = (int*)malloc(sizeof(int)* len+1);
+	int *new_tab = (int*)malloc(sizeof(int)* length+1);
 	new_tab[len] = 0;
 	while(i < length)
 	{
 		tab[i] = f(tab[i]);
 	i++;
 	}
-	tab[i] = 0;
-	return (tab);
+	return (new_tab);
 }
