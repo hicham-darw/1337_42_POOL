f9c53ad75fdd82752eecd9ad998c8569b3c1c122
hicham-darw <hamoudhicham234@gmail.com>
2025-07-04 14:58:53 +0100

    cached files .gitignore

--- shell00/ex06/git_ignore.sh
+++ shell00/ex06/git_ignore.sh
@@ -1,3 +1,10 @@
-git  --ignored ./
+touch .gitignore
 
+echo ".DS_store" > .gitignore
+echo "my_work.c-" > .gitignore
+
+git rm --cached .
+git add . 
+git commit -m "cached files .gitignore"
+git push
 
