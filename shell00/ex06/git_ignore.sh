2f52637a247a0568ec6c203971ad35991114c2b3
hicham-darw <hamoudhicham234@gmail.com>
2025-07-04 16:09:13 +0100

    script shell

--- shell00/ex06/git_ignore.sh
+++ shell00/ex06/git_ignore.sh
@@ -1,10 +1,1 @@
-touch .gitignore
-
-echo ".DS_store" > .gitignore
-echo "my_work.c-" >> .gitignore
-
-git rm -rf --cached .
-git add . 
-git commit -m "cached files .gitignore"
-git push
-
+git check-ignore 
