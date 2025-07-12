6b342347d16878d807b866419669a2c02ecd2c95
hicham-darw <hamoudhicham234@gmail.com>
2025-07-12 19:33:52 +0100

    fix by norminette

--- day07/ex00/ft_strdup.c
+++ day07/ex00/ft_strdup.c
@@ -4,19 +4,19 @@ int	ft_strlen(char *str);
 
 char	*ft_strdup(char* src)
 {
-	char *ptr ;
-	int i;
+	char	*ptr;
+	int	i;
 
-	ptr = malloc(sizeof(char) * ft_strlen(src)+1);
-	if( !ptr )
-		return NULL;
+	ptr = (char *)malloc(sizeof(char) * ft_strlen(src) + 1);
+	if (!ptr)
+		return (NULL);
 	i = 0;
-	while(src[i])
+	while (src[i])
 	{
 		ptr[i] = src[i];
 		i++;
 	}
 	ptr[i] = '\0';
 	
-	return ptr;
+	return (ptr);
 }
