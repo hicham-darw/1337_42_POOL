f9e070b923c5c657eaa4d75806dcf3757eb070fb
hicham-darw <hamoudhicham234@gmail.com>
2025-07-12 19:34:14 +0100

    by norminette

--- day07/ex01/ft_range.c
+++ day07/ex01/ft_range.c
@@ -5,9 +5,10 @@ int	*ft_range(int min, int max)
 	int	*array;
 	int	i;
 
-	array = (int *)malloc(sizeof(int) * (max - min));
+	array = (int *)malloc(sizeof(int) * (max - min) + 1);
 	if (min >= max)
 		return (NULL);
+	array[max-min] = 0;
 	i = 0;
 	while (min < max)
 	{
